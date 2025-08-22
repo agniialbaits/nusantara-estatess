7468da52ebedbe8817871e604ef71dbe0a10b3bc
Agnia Albaitsah <agnialatas1@gmail.com>
2025-08-22 15:15:57 +0700

    first commit

--- scripts/start-frontend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo 🎨 Starting Frontend Development Server...
-echo.
-echo Server will run on: http://localhost:5173
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-node node_modules/vite/bin/vite.js
-pause
